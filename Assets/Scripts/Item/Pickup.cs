ef2036d59ba5e7e089742a37438ffc456db48a2e
josephquested <josephquested@gmail.com>
2017-01-19 15:57:51 +1300

    add lockpick sprite

---
+++ Assets/Scripts/Item/Pickup.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+
+	public string type;
+
+	void OnTriggerEnter2D (Collider2D collider) {
+		if (collider.tag == "Player") {
+			Destroy(gameObject);
+		}
+	}
+}
