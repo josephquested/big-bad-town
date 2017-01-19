8f0fe2931cb71fd5bcdac6dfc338cc150ad3215d
josephquested <josephquested@gmail.com>
2017-01-19 16:56:01 +1300

    working lockpick

--- Assets/Scripts/Item/Pickup.cs
+++ Assets/Scripts/Item/Pickup.cs
@@ -5,11 +5,27 @@ using UnityEngine;
 public class Pickup : MonoBehaviour {
 
 	public string type;
+	public bool respawns = true;
+
+	void Awake () {
+		if (!respawns && PlayerPrefs.GetInt(gameObject.name) == 1) {
+			Destroy(gameObject);
+		}
+	}
 
 	void OnTriggerEnter2D (Collider2D collider) {
 		if (collider.tag == "Player") {
-			collider.GetComponent<PlayerInventory>().PickupLockpick();
-			Destroy(gameObject);
+			if (type == "lockpick") {
+				collider.GetComponent<PlayerInventory>().PickupLockpick();
+			}
+			GetPickup();
 		}
+	}
+
+	void GetPickup () {
+		if (!respawns) {
+			PlayerPrefs.SetInt(gameObject.name, 1);
+		}
+		Destroy(gameObject);
 	}
 }
