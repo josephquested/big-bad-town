24002c18912e60fb8296668a4e1b33f900e816eb
josephquested <josephquested@gmail.com>
2017-01-20 19:14:55 +1300

    initial meat work

--- Assets/Scripts/Game/HUD/HUDMeatController.cs
+++ Assets/Scripts/Game/HUD/HUDMeatController.cs
@@ -2,50 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HUDMeatController : MonoBehaviour
-{
-	// private PlayerStatus playerStatus;
-	//
-	// [SerializeField] private HUDMeat[] hudMeat;
-	//
-	// void Start ()
-	// {
-	// 	playerStatus = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStatus>();
-	// 	UpdateBones();
-	// }
-	//
-	// void Update ()
-	// {
-	// 	UpdateMeat();
-	// }
-	//
-	// void UpdateBones ()
-	// {
-	// 	for (int i = 1; i < hudMeat.Length; i++)
-	// 	{
-	// 		hudMeat[i].transform.localPosition = new Vector2(
-	// 			hudMeat[i - 1].transform.localPosition.x + 1,
-	// 			0
-	// 		);
-	// 	}
-	// }
-	//
-	// void UpdateMeat ()
-	// {
-	// 	int health = playerStatus.Health;
-	//
-	// 	for (int i = 0; i < hudMeat.Length; i++)
-	// 	{
-	// 		hudMeat[i].Plump = 0;
-	//
-	// 		for (int j = 0; j < 3; j++)
-	// 		{
-	// 			if (health > 0)
-	// 			{
-	// 				hudMeat[i].Plump++;
-	// 				health--;
-	// 			}
-	// 		}
-	// 	}
-	// }
+public class HUDMeatController : MonoBehaviour {
+	private Status status;
+
+	[SerializeField] private HUDMeat[] hudMeat;
+
+	void Start () {
+		status = GameObject.FindGameObjectWithTag("Player").GetComponent<Status>();
+		UpdateBones();
+	}
+
+	void Update () {
+		UpdateMeat();
+	}
+
+	void UpdateBones () {
+		for (int i = 1; i < hudMeat.Length; i++) {
+			hudMeat[i].transform.localPosition = new Vector2(hudMeat[i - 1].transform.localPosition.x + 1, 0);
+		}
+	}
+
+	void UpdateMeat () {
+		int health = status.health;
+
+		for (int i = 0; i < hudMeat.Length; i++)
+		{
+			hudMeat[i].plump = 0;
+
+			for (int j = 0; j < 3; j++)
+			{
+				if (health > 0)
+				{
+					hudMeat[i].plump++;
+					health--;
+				}
+			}
+		}
+	}
 }
