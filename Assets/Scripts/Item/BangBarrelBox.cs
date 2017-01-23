4231799eb32a4bca946fdbe7391f0689e12b6d4d
josephquested <josephquested@gmail.com>
2017-01-23 14:32:13 +1300

    rolling bang barrel

---
+++ Assets/Scripts/Item/BangBarrelBox.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BangBarrelBox : MonoBehaviour {
+	public GameScreen parentScreen;
+	bool cooledDown = true;
+
+	public GameObject barrelPrefab;
+	public int direction;
+	public float frequency;
+	public float velocity;
+
+	void Start () {
+		parentScreen = transform.parent.gameObject.GetComponent<GameScreen>();
+	}
+
+	void Update () {
+		if (parentScreen.screenActive) {
+			if (cooledDown) {
+				Spawn();
+			}
+		}
+	}
+
+	void Spawn () {
+		var prefab = Instantiate(barrelPrefab, transform.position, transform.rotation);
+		prefab.GetComponent<Rigidbody2D>().AddForce(GetVector(direction) * velocity);
+		print(prefab);
+		StartCoroutine(CooldownCoroutine());
+	}
+
+	IEnumerator CooldownCoroutine () {
+		cooledDown = false;
+		yield return new WaitForSeconds(frequency);
+		cooledDown = true;
+	}
+
+	Vector2 GetVector (int direction) {
+		if (direction == 0) return new Vector2(0, 1);
+		if (direction == 1) return new Vector2(1, 0);
+		if (direction == 2) return new Vector2(0, -1);
+		return new Vector2(-1, 0);
+	}
+}
--- Assets/Scripts/Item/Explodes.cs
+++ Assets/Scripts/Item/Explodes.cs
@@ -8,12 +8,16 @@ public class Explodes : MonoBehaviour {
 	public Collider2D explosionCollider;
 	public AudioClip explodeSound;
 	public bool explosionOnly;
+	public bool onTouch;
 
 	void Start () {
 		audioSource = GetComponent<AudioSource>();
 	}
 
 	public void Explode () {
+		if (GetComponent<Rigidbody2D>() != null) {
+			GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+		}
 		StartCoroutine(ExplodeCoroutine());
 	}
 
@@ -24,5 +28,23 @@ public class Explodes : MonoBehaviour {
 		explosionCollider.enabled = true;
 		yield return new WaitForSeconds(0.5f);
 		Destroy(gameObject);
+	}
+
+	void OnTriggerEnter2D (Collider2D collider) {
+		if (onTouch) {
+			if (collider.tag == "Player" || collider.tag == "Enemy") {
+				Explode();
+			}
+
+			if (collider.gameObject.GetComponent<Explodes>() != null) {
+				Explodes explodes = collider.gameObject.GetComponent<Explodes>();
+				explodes.Explode();
+				Explode();
+			}
+
+			if (collider.gameObject.layer == LayerMask.NameToLayer("Solid")) {
+				Explode();
+			}
+		}
 	}
 }
