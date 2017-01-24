74c11e81399192022e06473279ff723270578c61
josephquested <josephquested@gmail.com>
2017-01-24 17:27:04 +1300

    fix boss respawn bug

--- Assets/Scripts/Player/Inventory.cs
+++ Assets/Scripts/Player/Inventory.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Inventory : MonoBehaviour {
 	public int lockpicks;
 	public int cash;
+	public int bullets;
 
 	public void Pickup (string type, int quantity) {
 		if (type == "lockpick") {
@@ -16,6 +17,16 @@ public class Inventory : MonoBehaviour {
 		if (type == "meat") {
 			PlayerStatus status = GetComponent<PlayerStatus>();
 			status.IncreaseMaxHealth();
+		}
+		if (type == "pistol") {
+			RangedAttack rangedAttack = GetComponent<RangedAttack>();
+			if (rangedAttack.weapon == null) {
+				RangedWeapon pistol = transform.Find("Pistol").gameObject.GetComponent<RangedWeapon>();
+				rangedAttack.weapon = pistol;
+				bullets += 10;
+			} else {
+				bullets += 2;
+			}
 		}
 	}
 
