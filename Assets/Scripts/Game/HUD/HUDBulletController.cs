0f31115bbc0a56c97bdcff4ea5c6713d3047d5e6
josephquested <josephquested@gmail.com>
2017-01-24 20:42:29 +1300

    working bullet counter

---
+++ Assets/Scripts/Game/HUD/HUDBulletController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDBulletController : MonoBehaviour {
+	private Inventory inventory;
+
+	public List<Sprite> numberSprites;
+	public SpriteRenderer tensSprite;
+	public SpriteRenderer onesSprite;
+
+	void Start () {
+		inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+		UpdateBullets();
+	}
+
+	void Update () {
+		UpdateBullets();
+	}
+
+	void UpdateBullets () {
+		int tens = Mathf.Abs(inventory.bullets);
+		while (tens >= 10) {
+			tens /= 10;
+		}
+		int ones = inventory.bullets % 10;
+		tensSprite.sprite = numberSprites[tens];
+		onesSprite.sprite = numberSprites[ones];
+		if (inventory.bullets < 10) {
+			tensSprite.sprite = numberSprites[0];
+		}
+	}
+}
